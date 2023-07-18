da9a7a34b530f20488f0b1e60ee776f0580e4d61
보니 <73548875+Guboneui@users.noreply.github.com>
2023-07-19 04:03:05 +0900

    Localization Test with String Extension and Struct

--- LocalizationTest/ViewController.swift
+++ LocalizationTest/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
+extension String {
+  func localized(comment: String = "") -> String {
+    return NSLocalizedString(self, comment: comment)
   }
-
-
 }
 
+struct LocalizedString {
+  static let TEST: String = "TEST"
+  static let Hello: String = "Hello".localized()
+}
+
+
+class ViewController: UIViewController {
+
+  
+  private let label: UILabel = UILabel()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = .white
+    self.setupLabel()
+    self.setupLayouts()
+    
+    DispatchQueue.main.asyncAfter(deadline: .now()+3, execute: { [weak self] in
+      guard let self else { return }
+      self.label.text = LocalizedString.TEST.localized()
+      self.label.sizeToFit()
+    })
+  }
+  
+  private func setupLabel() {
+    label.font = UIFont.systemFont(ofSize: 20)
+    label.text = LocalizedString.Hello
+    label.sizeToFit()
+  }
+  
+  private func setupLayouts() {
+    self.view.addSubview(label)
+    label.frame.origin.x = 150
+    label.frame.origin.y = 150
+  }
+}
